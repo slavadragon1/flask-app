ac52c6dbccd088d23bd4e995d2f106c3ca3cbd23
slava ivanov <slava.a.ivanov@gmail.com>
2023-06-20 13:35:29 +0300

    adding socketio

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from src import app
+from flask import render_template, url_for
+from flask_socketio import SocketIO, emit
 
+socketio = SocketIO(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello world'
+    user = {'username': 'Slava'}
+    return render_template('index.html', user = user )
+
+@socketio.on('message')
+def handle_message(message):
+    emit('message', message, broadcast=True)
