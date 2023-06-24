a1b1bed537311d308d50a87483ea2fba04212b9e
slava ivanov <slava.a.ivanov@gmail.com>
2023-06-24 12:59:46 +0300

    add wtforms

--- src/routes.py
+++ src/routes.py
@@ -3,14 +3,19 @@
 from src import app
 from flask import render_template, url_for
 from flask_socketio import SocketIO, emit
+from forms import LoginForm
 
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-    user = {'username': 'Slava'}
-    return render_template('index.html', user = user )
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 @socketio.on('message')
 def handle_message(message):
