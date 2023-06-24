1b04133231ce665a852b186ae64a342ea40759be
slava ivanov <slava.a.ivanov@gmail.com>
2023-06-24 13:25:27 +0300

    wtforms fix

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from src import app
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
 from flask_socketio import SocketIO, emit
-from forms import LoginForm
+from src.forms import LoginForm
 
 socketio = SocketIO(app)
 
@@ -12,10 +12,16 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
 
 @socketio.on('message')
 def handle_message(message):
